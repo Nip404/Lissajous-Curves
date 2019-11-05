2cdc3774be7a0de0d1f6a1a575ae4efdca02bb97
Nip404 <57417504+Nip404@users.noreply.github.com>
2019-11-05 23:48:05 +0000

    Update and rename main.py to lissajous.py

--- lissajous.py
+++ lissajous.py
@@ -1,45 +1,45 @@
 from engine import *
 import pygame
 
-s = [800,800]
+s = [800, 800]
 
 pygame.init()
-screen = pygame.display.set_mode(s,0,32)
+screen = pygame.display.set_mode(s, 0, 32)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Lissajous curve animations by NIP")
 
-def get_circles(x_amount,y_amount):
+def get_circles(x_amount, y_amount):
     x_amount += 1
     y_amount += 1
     
-    x_rad = int(screen.get_width()/(x_amount*2))
-    y_rad = int(screen.get_height()/(y_amount*2))
+    x_rad = int(screen.get_width() / (x_amount*2))
+    y_rad = int(screen.get_height() / (y_amount*2))
 
     if x_rad > y_rad:
-        y_rad = int((screen.get_height()-(2*x_rad))/(y_amount*2))
+        y_rad = int((screen.get_height() - (2*x_rad)) / (y_amount*2))
         x_start = y_rad
-        y_start = 2*x_rad
+        y_start = 2 * x_rad
         
     elif y_rad > x_rad:
-        x_rad = int((screen.get_width()-(2*y_rad))/(x_amount*2))
+        x_rad = int((screen.get_width() - (2*y_rad)) / (x_amount*2))
         
-        x_start = 2*y_rad
+        x_start = 2 * y_rad
         y_start = x_rad
         
     else:
         x_start = x_rad
         y_start = y_rad
         
-    x = [Circle([x_start+(p*2*x_rad),x_rad],x_rad,p/3) for p in range(x_amount)]
-    y = [Circle([y_rad,y_start+(p*2*y_rad)],y_rad,p/3) for p in range(y_amount)]
+    x = [Circle([x_start + (p * 2 * x_rad),x_rad], x_rad, p/3) for p in range(x_amount)]
+    y = [Circle([y_rad, y_start + (p * 2 * y_rad)], y_rad, p/3) for p in range(y_amount)]
 
-    return x[1:],y[1:]
+    return x[1:], y[1:]
 
 def main():
     display = Display(screen)
-    x_amount,y_amount = 5,5
+    x_amount, y_amount = 5, 5
 
-    x,y = get_circles(x_amount,y_amount)
+    x, y = get_circles(x_amount, y_amount)
 
     display.init_x_circles(x)
     display.init_y_circles(y)
@@ -51,7 +51,7 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-        screen.fill((0,0,0))
+        screen.fill((0, 0, 0))
 
         display.draw_sources()
         display.draw_lines()
